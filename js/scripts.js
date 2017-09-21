fae9c1f539a7479a01c3de4edf0795e149530a61
Adam Titus and Rane Fields <adam+rane@>
2017-09-21 11:58:08 -0700

    add roman numeral to decimal functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
 var conversionMap = {
   M: 1000,
+  CM: 900,
   D: 500,
+  CD: 400,
   C: 100,
+  XC: 90,
   L: 50,
+  XL: 40,
   X: 10,
+  IX: 9,
   V: 5,
+  IV: 4,
   I: 1
 };
 var keys = Object.keys(conversionMap);
 
 function toRoman(userInput) {
-  if (userInput > 3999) {
+  if (userInput > 3999 || /[^\d]/.test(userInput)) {
     return null;
   }
   var romanArray = [];
@@ -24,23 +30,56 @@ function toRoman(userInput) {
     }
   }
   output = romanArray.join("");
-  output = output.replace(/V[I]{4}/g, "IX");
-  output = output.replace(/[I]{4}/g, "IV");
-  output = output.replace(/L[X]{4}/g, "XC");
-  output = output.replace(/[X]{4}/g, "XL");
-  output = output.replace(/D[C]{4}/g, "CM");
-  output = output.replace(/[C]{4}/g, "CD");
   return output;
 };
 
+function toDecimal(userInput) {
+  userInput = userInput.toUpperCase();
+  var output = 0;
+  if (/[^IVXLCDM]/.test(userInput)) {
+    return null;
+  }
+  if (/(I{4}|V{2}|X{4}|L{2}|C{4}|D{2}|M{4})/.test(userInput)) {
+    return null;
+  }
+  if (/^M*(CM)?((D?C*)|(CD)?)(XC)?((L?X*)|(XL)?)(IX)?((V?I*)|(IV)?)$/.test(userInput)) {
+    for (i = 0; i < keys.length; i++) {
+      var currentKey = keys[i];
+      var currentChunk = userInput.substring(0, currentKey.length);
+
+      while (currentKey === currentChunk) {
+        userInput = userInput.substring(currentKey.length);
+        output += conversionMap[currentKey];
+        currentChunk = userInput.substring(0, currentKey.length)
+      }
+    }
+    return output;
+
+  } else {
+    return null;
+  }
+};
+
 $(document).ready(function(){
-  $("form#roman").submit(function(event){
+  $("form#to-roman").submit(function(event){
     event.preventDefault();
-    var userInput = parseInt($("#userInput").val());
-    if (toRoman(userInput) === null) {
+    var userInput = $("#to-roman-input").val();
+    var output = toRoman(userInput);
+    if (output === null) {
       $(".output").text("no");
     } else {
-      $(".output").text(toRoman(userInput));
+      $(".output").text(output);
+    }
+  });
+
+  $("form#to-decimal").submit(function(event){
+    event.preventDefault();
+    var userInput = $("#to-decimal-input").val();
+    var output = toDecimal(userInput);
+    if (output === null) {
+      $(".output").text("no");
+    } else {
+      $(".output").text(output);
     }
   });
 });
